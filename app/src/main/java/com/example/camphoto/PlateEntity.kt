4bab65ad4ff49610c33280ebd25399c74ba10d98
grenadinraf-droid <grenadinraf@gmail.com>
2026-08-04 14:53:03 +0300

    Create PlateEntity.kt

---
+++ app/src/main/java/com/example/camphoto/PlateEntity.kt
@@ -0,0 +1,12 @@
+package com.example.camphotolpr
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "license_plates")
+data class PlateEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val plateNumber: String,
+    val timestamp: Long = System.currentTimeMillis()
+)
