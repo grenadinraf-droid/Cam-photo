9c4e47f9ef27a4c60a460e04756cda06c1ab33ab
grenadinraf-droid <grenadinraf@gmail.com>
2026-08-04 14:54:54 +0300

    Create AppDatabase.kt

---
+++ app/src/main/java/com/example/camphoto/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.camphotolpr
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [PlateEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun plateDao(): PlateDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "license_plates_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
