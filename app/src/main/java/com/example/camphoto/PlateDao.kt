159b7d02b51418cfb9f0089b964d7a9296c9c4b5
grenadinraf-droid <grenadinraf@gmail.com>
2026-08-04 14:54:06 +0300

    Create PlateDao.kt

---
+++ app/src/main/java/com/example/camphoto/PlateDao.kt
@@ -0,0 +1,16 @@
+package com.example.camphotolpr
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PlateDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(plate: PlateEntity)
+
+    @Query("SELECT * FROM license_plates ORDER BY timestamp DESC")
+    fun getAllPlates(): Flow<List<PlateEntity>>
+}
