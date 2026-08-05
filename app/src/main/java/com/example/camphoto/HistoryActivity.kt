b521979e57626b32185cc935abdcc4549d0cd6e8
grenadinraf-droid <grenadinraf@gmail.com>
2026-08-05 22:33:26 +0300

    Create HistoryActivity.kt

---
+++ app/src/main/java/com/example/camphoto/HistoryActivity.kt
@@ -0,0 +1,27 @@
+package com.example.camphotolpr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        val rvHistory = findViewById<RecyclerView>(R.id.rvHistory)
+        rvHistory.layoutManager = LinearLayoutManager(this)
+
+        val database = AppDatabase.getDatabase(this)
+
+        lifecycleScope.launch {
+            database.plateDao().getAllPlates().collect { list ->
+                rvHistory.adapter = PlateAdapter(list)
+            }
+        }
+    }
+}
