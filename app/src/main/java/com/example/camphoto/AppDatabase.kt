74319ba05ed98b58ec509a2f0c673d3b5198e92a
grenadinraf-droid <grenadinraf@gmail.com>
2026-08-06 19:14:27 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/camphoto/AppDatabase.kt
+++ app/src/main/java/com/example/camphoto/AppDatabase.kt
@@ -5,9 +5,15 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [PlateEntity::class], version = 2, exportSchema = false)
+@Database(
+    entities = [PlateEntity::class, PlateRuleEntity::class], // Добавили сущность правил
+    version = 2, 
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
+    
     abstract fun plateDao(): PlateDao
+    abstract fun plateRuleDao(): PlateRuleDao // Добавили DAO для черного/белого списков
 
     companion object {
         @Volatile
@@ -20,7 +26,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "license_plates_db"
                 )
-                .fallbackToDestructiveMigration()
+                .fallbackToDestructiveMigration() // Очищает и пересоздает базу при смене версии
                 .build()
                 INSTANCE = instance
                 instance
